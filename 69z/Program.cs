87fd63c6098f966799f4650d90abbf3c160f6f28
Alexandr Kuzmin <kuzmin_89@mail.ru>
2023-01-14 00:32:27 +0500

    69z

---
+++ 69z/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите M: ");
+int m = int.Parse(ReadLine());
+Write("Введите N: ");
+int n = int.Parse(ReadLine());
+WriteLine(PrintNumbers(m, n));
+
+int PrintNumbers(int a, int b)
+{
+    if (b == 0) return 1;
+    return (a * PrintNumbers(a, b - 1));
+}
