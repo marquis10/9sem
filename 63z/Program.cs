95159567f6b1bbcdee73b9d36d538d6514b0c813
Alexandr Kuzmin <kuzmin_89@mail.ru>
2023-01-14 11:34:04 +0500

    69zz

---
+++ 63z/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите N: ");
+int n=int.Parse(ReadLine());
+
+WriteLine(PrintNumbers(n));
+
+string PrintNumbers(int end)
+{
+    if (end == 1)
+    {
+        Console.WriteLine(1);
+        return "1";
+    }
+    string s = PrintNumbers(end - 1) + ' ' + end.ToString();
+    Console.WriteLine(s);
+    return (s);
+}
---
+++ 67z/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите N: ");
+int n=int.Parse(ReadLine());
+WriteLine(PrintNumbers(n));
+int PrintNumbers(int sum)
+{
+    if (sum == 0)
+        return 0;
+    return (sum % 10 + PrintNumbers(sum / 10));
+}
